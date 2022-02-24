4123255d8ac2534c527a57173db71592b315178f
Kevin Shu <shu.kevins@gmail.com>
2022-02-24 14:39:21 -0500

    A heuristic

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
-file = "a.txt"
+filename = "c.txt"
 contributors = dict()
 skills = set()
 projects = dict()
 project_skills = dict()
 final_deadline = -1
-with open(file) as f:
+with open(filename) as f:
     ncontributors, nprojects = [int(x) for x in f.readline().split(" ")]
     # Get each contributor and the skills they have
     for i in range(ncontributors):
@@ -16,7 +16,7 @@ with open(file) as f:
             level = int(level)
             contributors[name][skill] = level
             skills.add(skill)
-    # backfill the skills that weren't listed for each contributor
+    # backfill the skills that weren't listed for bach contributor
     for contributor in contributors:
         for skill in skills:
             if skill not in contributors[contributor]:
@@ -30,18 +30,14 @@ with open(file) as f:
         nroles = int(nroles)
         final_deadline = max(final_deadline, bestby + score)
         projects[name] = (days, score, bestby, nroles)
-        project_skills[name] = dict()
+        project_skills[name] = list()
         for i in range(nroles):
             skill, level = f.readline().split(" ")
-            project_skills[name][skill] = int(level)
-print(contributors)
-print(skills)
-print(projects)
-print(project_skills)
+            project_skills[name].append((skill, int(level)))
 
 def is_possible(project_roles, available_workers):
     workers = list(available_workers)
-    adj = [[1 if can_work(worker, (role, project_roles[role])) else 0 for worker in workers] for role in project_roles]
+    adj = [[1 if can_work(worker, role) else 0 for role in project_roles] for worker in workers]
     g = GFG(adj)
     n, assignment = g.maxBPM()
     if n < len(project_roles):
@@ -111,17 +107,27 @@ class GFG:
         return result, matchR
  
 # This code is contributed by Neelam Yadav
-
+output = open("out."+filename, "w")
 all_coders = contributors.keys()
-available_coders = [set(contributors.keys()) for i in range(final_deadline+1)]
+earliest_available = {coder: 0 for coder in all_coders}
+print("made_coders_available")
 completed_days = dict()
+n_projects = 0
+final_deadline = 5000
 for day in range(final_deadline+1):
+    available_coders = [coder for coder in all_coders if earliest_available[coder] >= day]
+    if len(projects) == 0:
+        break
+    print("{} out of {}".format(day, final_deadline))
     # Find the best project at each day
     best_score = -1
     best_project = ""
+    best_workers_needed = []
+    if len(projects) == 0:
+        break
     for project in projects:
         # determine if the project is possible, and if so, which workers are needed
-        project_possible, workers_needed = is_possible(project_skills[project], available_coders[day])
+        project_possible, workers_needed = is_possible(project_skills[project], available_coders)
         days_needed, score, bestby, nroles = projects[project]
         if project_possible:
             # score project
@@ -129,20 +135,29 @@ for day in range(final_deadline+1):
             value = (score + penalty)/days_needed
             if value == 0:
                 continue
-            if score > best_score:
+            if value > best_score:
                 best_score = score
                 best_project = project
-        else:
-            continue
-        # Make the workers unavailable for the days of the project.
-        for i in range(days_needed):
-           available_coders[day + i] = available_coders[day + i].difference(set(workers_needed))
-        completion_date = day + days_needed
-        if completion_date not in completed_days:
-            completed_days[completion_date] = []
-        completed_days[completion_date].append((project, workers_needed))
+                best_workers_needed = workers_needed
+    if best_project == "":
+        continue
+    n_projects += 1
+    print(best_project)
+    output.write(best_project + "\n")
+    output.write(" ".join(best_workers_needed) + "\n")
+    days_needed, score, bestby, nroles = projects[best_project]
+    # Make the workers unavailable for the days of the project.
+    for worker in best_workers_needed:
+        earliest_available[worker] = day + days_needed
+    completion_date = day + days_needed
+    if completion_date not in completed_days:
+        completed_days[completion_date] = []
+    completed_days[completion_date].append((best_project, best_workers_needed))
+    del projects[best_project]
+    # update worker skills
     if day in completed_days:
         for project, workers in completed_days[day]:
             for worker, role in zip(workers, project_skills[project]):
-                if contributors[worker][role] <= project_skills[project][role]:
-                    contributors[worker][role] += 1
+                if contributors[worker][role[0]] <= role[1]:
+                    contributors[worker][role[0]] += 1
+print(n_projects)
